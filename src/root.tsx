d931b6f6d32e971b3799e7ee31a56ebad7278ea8
Tandashi <18377875+Tandashi@users.noreply.github.com>
2022-09-29 18:43:46 +0200

    fix: `@media` did not work on mobile devices
    Reference: https://stackoverflow.com/questions/17344339/media-query-not-working-in-mobile-works-fine-in-chrome

--- src/root.tsx
+++ src/root.tsx
@@ -21,6 +21,7 @@ export default component$(() => {
     <QwikCity>
       <head>
         <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1.0'></meta>
       </head>
       <body lang='en' class={['flex w-screen h-screen', themeStore.mode === 'dark' ? 'dark' : '']}>
         <RouterOutlet />
