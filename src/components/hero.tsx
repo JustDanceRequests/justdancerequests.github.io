100f76d8996587e3ee60a55a392532f2885470c6
Tandashi <18377875+Tandashi@users.noreply.github.com>
2022-10-03 18:59:27 +0200

    style: Hero mobile alignments

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import { component$ } from '@builder.io/qwik';
 
 export const Hero = component$(() => {
   return (
-    <div class='flex flex-col md:flex-row w-full'>
+    <div class='flex flex-col md:flex-row w-full space-y-4 md:space-y-0'>
       <div class='flex-1 flex flex-col items-center justify-center md:ml-44'>
         <div class='flex-1 flex flex-col items-start justify-center space-y-16'>
           <div class='font-bold text-7xl'>
@@ -12,7 +12,7 @@ export const Hero = component$(() => {
           </div>
         </div>
       </div>
-      <div class='flex flex-col flex-1 items-start justify-center space-y-16'>
+      <div class='flex flex-col flex-1 items-center md:items-start justify-center space-y-8 md:space-y-16'>
         <div class='font-light text-3xl'>
           <p class='text-white'>
             Making requesting songs <br />
